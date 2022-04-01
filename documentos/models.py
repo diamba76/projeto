3e599f040ec1bf4ac066fdf9cdcce9056e545bf9
diamba76 <102378802+diamba76@users.noreply.github.com>
2022-04-01 10:06:46 -0300

    Update

--- documentos/models.py
+++ documentos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Documento(models.Model):
     descricao = models.CharField(max_length=200, blank=True)
-    documento = models.FileField(upload_to='documents/')
+    documento = models.FileField(upload_to='documentos/')
 
     def __str__(self):
         return self.descricao
